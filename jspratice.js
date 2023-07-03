777bca6de33c0eecceee8a8c87cc82b59db43c74
Sheshank2805 <danthanapallysheshank123@gmail.com>
2023-07-03 16:50:28 +0530

    no of vowels

---
+++ jspratice.js
@@ -0,0 +1,27 @@
+function counting(a){
+    var count=0;
+  for(i=0;a[i]!=undefined;i++){
+      count+=1;
+     
+  }
+  return count;
+}
+console.log(counting("Sheshank"));
+console.log(counting("Good Morning"));
+
+
+//counting no of vowels
+debugger
+function countingvowels(a){
+    
+    count=0;
+  for(i=0;i<a.length;i++){
+      
+    if (a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')  {
+      count++;
+    }
+  }
+return count;
+}
+let noOfvowels=countingvowels("This is a program to count no of vowels in a string");
+console.log("Total no of vowels in the given word are:"+noOfvowels);
